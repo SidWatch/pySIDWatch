7eff80216125f26def3ab22dd3d84d3a375ac31e
Brian Nelson <brian@treegecko.com>
2014-09-15 21:49:30 -0600

    Setting up structure for build

---
+++ Source/setup.py
@@ -0,0 +1,29 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+options = {'build_exe': {
+    'packages': ['os', 'yaml', 'boto', 'numpy', 'scipy', 'matplotlib', 'io', 'zlib',
+                 'zipfile', 'bz2', 'math', 'datetime', 'dateutil', 'time', 'h5py',
+                 'pyaudio', 'sys'],
+    'include_files': ['Audio', 'SID/', 'SIDClient', 'SIDRest'],
+    'excludes': [],
+    "build_exe": '../build/Output/Win32/'
+}
+}
+
+executable_to_build = [
+    Executable('SendToSidWatchServer.py',
+               base='console',
+               targetDir='../build/Output/Win32/'),
+    Executable('SidDataCollector.py',
+               base='console',
+               targetDir='../build/Output/Win32/')
+]
+
+setup(name='pySidWatch',
+      version='0.1.0.0',
+      description='SidWatch Data Collector',
+      options=options,
+      executables=executable_to_build
+)
